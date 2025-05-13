3736295fd36c5757cc4e9abdd227ba760da2a1b7
AsaruAsharudheen <asaru313313@gmail.com>
2025-05-14 01:12:53 +0530

    add App

--- src/App.jsx
+++ src/App.jsx
@@ -14,32 +14,33 @@ const App = () => {
   };
 
   const downloadAsPNG = () => {
-    if (posterRef.current) {
-      html2canvas(posterRef.current, { useCORS: true }).then((canvas) => {
-        const link = document.createElement('a');
-        link.download = 'poster.png';
-        link.href = canvas.toDataURL('image/png');
-        link.click();
-      });
-    }
+    if (!posterRef.current) return;
+
+    html2canvas(posterRef.current, { useCORS: true, scale: 2 }).then((canvas) => {
+      const link = document.createElement('a');
+      link.download = 'poster.png';
+      link.href = canvas.toDataURL('image/png');
+      link.click();
+    });
   };
 
   return (
     <div className="poster-container">
       <div className="poster-wrapper" ref={posterRef}>
+        {/* Poster Background */}
         <img
           src="/Tag W Photo Official_.png"
           alt="Poster Background"
           className="poster-bg"
         />
 
-        {/* Upload area */}
-        <label className="upload-area second">
+        {/* Image Upload Area */}
+        <label className="upload-area">
           {uploadedImage ? (
             <img
               src={uploadedImage}
               alt="Uploaded"
-              className="image-layer second"
+              className="uploaded-image"
             />
           ) : (
             <span className="upload-text">Click to upload image</span>
@@ -52,14 +53,14 @@ const App = () => {
           />
         </label>
 
-        {/* Editable Text */}
+        {/* Editable Text Fields */}
         <p
-          className="custom-text-second"
+          className="text-primary"
           contentEditable
           suppressContentEditableWarning
         ></p>
         <p
-          className="custom-text-third"
+          className="text-secondary"
           contentEditable
           suppressContentEditableWarning
         ></p>
